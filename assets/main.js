ba644d10401ed9b24f422bf8714c63a5b2855170
Justin Rice <jsrice7391@gmail.com>
2018-02-02 08:11:29 -0500

    Some styling and some pseudo

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,4 @@
+// This goes through all of the buttons on the UI and shows the buttons from the user choices array.
 function show_all_buttons(array) {
     $("#buttons").empty();
     $.each(array, function(index, search) {
@@ -5,21 +6,26 @@ function show_all_buttons(array) {
     })
 }
 
+// The original choices for the user to click and see the GIFs
 var user_choices = ["the office", "dogs", "cats", "pandas"];
 
+// This will be used to make the API call to populate the page with the GIFs
 function show_all_gifs(the_button) {
     $(".gif_display").empty();
     $.ajax({
         url: "http://api.giphy.com/v1/gifs/search?q='" + the_button + "'&api_key=IzBmA6Dpos0EdfXIav4LTX8PYpp03Nj6&limit=12",
         method: "GET"
     }).done(function(returned) {
+        // Establishes the returned object to be something easier to deal with.
         var the_gifs = returned.data;
-        console.log(the_gifs);
+        // GO through all of the data this is returned
         the_gifs.forEach(function(gif) {
-
+            // Make sure that the rating isn't R or PG-13
             if (gif.rating !== "r" && gif.rating !== "pg-13") {
+                // Make a DIV for the GIF to be appended
                 var gif_div = $("<div class='forGifs'>");
                 var gif_img = $("<img/>");
+                // Give all the attributes to the UI 
                 gif_img.attr({
                     src: gif.images.fixed_height_small.url,
                     data_still: gif.images.original_still.url,
@@ -27,30 +33,30 @@ function show_all_gifs(the_button) {
                     data_state: "animate"
 
                 });
-
-
-                console.log(gif.images.downsized_still.url);
+                // Append the new object to the UI
                 $(gif_div).append(gif_img);
+                // Add some text to the bottom that shows the Rating
                 $(gif_div).append("<h5>Rating: " + gif.rating.toUpperCase() + "</h5>");
-
-
                 $(".gif_display").append(gif_div);
             }
-
-
         })
     })
 };
 
 $(document).ready(function() {
+    // Populate the top of the page with the buttons
     show_all_buttons(user_choices);
 
+    // When we click on the button, show the gifs that have to do with that button.
     $("#buttons").on("click", "button", function() {
+        // Normalze the input of the buttons data.
         var search_term = $(this).data("name").split(' ').join('_');
         show_all_gifs(search_term);
 
     });
 
+
+    // Add the input in the search bar to the top of the UI
     $("#add_input").on("click", function(event) {
         event.preventDefault();
         var user_input = $("#user_added_term").val();
@@ -80,8 +86,5 @@ $(document).ready(function() {
 
 
     });
-
-
-
 
 });
